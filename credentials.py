3b183829a212b7f21258d3af7602acf3715b13bf
Alex Singleton <alexanderjsingleton@alexs-imac.cisco>
2017-09-13 17:32:31 -0500

    added

--- credentials.py
+++ credentials.py
@@ -1,9 +1,10 @@
 # Twitter App access keys for @user
 
 # Consume:
-CONSUMER_KEY    = ''
-CONSUMER_SECRET = ''
+CONSUMER_KEY    = 'KPo8YuPqaZD0JVBnPzCza7qg9'
+CONSUMER_SECRET = 'sdHWMtZ4wvE2y333Q159uv0cre8xEaZRzs6NVWPvDCfrLBKtsV'
 
 # Access:
-ACCESS_TOKEN  = ''
-ACCESS_SECRET = ''
+ACCESS_TOKEN  = '37799081-dj8LIgnsy57wV4iZifz3NNGpSvgcdePFLSTUkQ1Fx'
+ACCESS_SECRET = 'p0czkIUrip9QD5AM8WuKZqytXy3ahGfDOuphrjcHQZfQZ'
+
--- trumpet.py
+++ trumpet.py
@@ -9,3 +9,33 @@ from IPython.display import display
 import matplotlib.pyplot as plt
 import seaborn as sns
 plt.ion()
+
+# We import our access keys:
+from credentials import *    # This will allow us to use the keys as variables
+
+# API's setup:
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with our access keys provided.
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+# We create an extractor object:
+extractor = twitter_setup()
+
+# We create a tweet list as follows:
+tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
+print("Number of tweets extracted: {}.\n".format(len(tweets)))
+
+# We print the most recent 5 tweets:
+print("5 recent tweets:\n")
+for tweet in tweets[:5]:
+    print(tweet.text)
+    print()
