1804df112774b9c5db029fd20b967721bb2b1318
Alex Singleton <alexanderjsingleton@alexs-imac.cisco>
2017-09-13 16:33:39 -0500

    first commit

---
+++ trumpet.py
@@ -0,0 +1,11 @@
+# General:
+import tweepy           # To consume Twitter's API
+import pandas as pd     # To handle data
+import numpy as np      # For number computing
+import matplotlib 
+
+# For plotting and visualization:
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.ion()
